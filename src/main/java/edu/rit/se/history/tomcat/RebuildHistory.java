64ec90b7e8ad38040a549c3bec3f1df3a24663ca
Andy Meneely <andy.meneely@gmail.com>
2012-02-27 16:31:25 -0500

    refactoring for unit tests

--- src/main/java/edu/rit/se/history/tomcat/RebuildHistory.java
+++ src/main/java/edu/rit/se/history/tomcat/RebuildHistory.java
@@ -19,11 +19,24 @@ import edu.rit.se.history.tomcat.parse.VulnerabilitiesToFilesParser;
 public class RebuildHistory {
 	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(RebuildHistory.class);
 
-	private static File datadir;
+	private final DBUtil dbUtil;
+	private final Properties props;
+	private File datadir;
 
 	public static void main(String[] args) throws Exception {
-		Properties props = setUpProps();
-		DBUtil dbUtil = setUpDB(props);
+		new RebuildHistory().run();
+	}
+
+	public DBUtil getDbUtil() {
+		return dbUtil;
+	}
+
+	public RebuildHistory() throws Exception {
+		this.props = setUpProps();
+		this.dbUtil = setUpDB(props);
+	}
+
+	private void run() throws Exception {
 
 		rebuildSchema(dbUtil);
 		// loadSVNXML(dbUtil, props);
@@ -37,31 +50,31 @@ public class RebuildHistory {
 		log.info("Done.");
 	}
 
-	private static Properties setUpProps() throws IOException {
+	private Properties setUpProps() throws IOException {
 		Properties props = PropsLoader.getProperties("tomcathistory.properties");
 		DOMConfigurator.configure("log4j.properties.xml");
 		datadir = new File(props.getProperty("history.datadir"));
 		return props;
 	}
 
-	private static DBUtil setUpDB(Properties props) throws ClassNotFoundException {
+	private DBUtil setUpDB(Properties props) throws ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
 		DBUtil dbUtil = new DBUtil(props.getProperty("history.dbuser"), props.getProperty("history.dbpw"), props.getProperty("history.dburl"));
 		return dbUtil;
 	}
 
-	private static void rebuildSchema(DBUtil dbUtil) throws FileNotFoundException, SQLException, IOException {
+	private void rebuildSchema(DBUtil dbUtil) throws FileNotFoundException, SQLException, IOException {
 		log.info("Rebuilding database schema...");
 		dbUtil.executeSQLFile("sql/createTables.sql");
 	}
 
-	private static void loadSVNXML(DBUtil dbUtil, Properties props) throws Exception {
+	private void loadSVNXML(DBUtil dbUtil, Properties props) throws Exception {
 		log.info("Loading the SVN XML into database...");
 		String file = props.getProperty("history.svnlogxml");
 		new LoadSVNtoDB(dbUtil, new File(datadir, file)).run();
 	}
 
-	private static void loadFileListing(DBUtil dbUtil, Properties props) throws FileNotFoundException, SQLException {
+	private void loadFileListing(DBUtil dbUtil, Properties props) throws FileNotFoundException, SQLException {
 		log.info("Parsing release files for Tomcat 5.5.0...");
 		new FileListingParser().parse(dbUtil, new File(datadir, props.getProperty("history.filelisting.v5")), "5.5.0");
 		log.info("Parsing release files for Tomcat 6.0.0...");
@@ -70,26 +83,26 @@ public class RebuildHistory {
 		new FileListingParser().parse(dbUtil, new File(datadir, props.getProperty("history.filelisting.v7")), "7.0.0");
 	}
 
-	private static void loadGroundedTheoryResults(DBUtil dbUtil, Properties props) throws Exception {
+	private void loadGroundedTheoryResults(DBUtil dbUtil, Properties props) throws Exception {
 		log.info("Parsing grounded theory results...");
 		new GroundedTheoryResultsParser().parse(dbUtil, new File(datadir, props.getProperty("history.groundedtheory")));
 	}
 
-	private static void loadCVEs(DBUtil dbUtil, Properties props) throws Exception {
+	private void loadCVEs(DBUtil dbUtil, Properties props) throws Exception {
 		log.info("Parsing CVE details...");
 		new CVEsParser().parse(dbUtil, new File(datadir, props.getProperty("history.cves")));
 	}
 
-	private static void filterSVNLog(DBUtil dbUtil, Properties props) {
+	private void filterSVNLog(DBUtil dbUtil, Properties props) {
 		throw new IllegalStateException("unimplemented!");
 	}
 
-	private static void loadVulnerabilitiesToFiles(DBUtil dbUtil, Properties props) throws Exception {
+	private void loadVulnerabilitiesToFiles(DBUtil dbUtil, Properties props) throws Exception {
 		log.info("Parsing CVE to Files...");
 		new VulnerabilitiesToFilesParser().parse(dbUtil, new File(datadir, props.getProperty("history.cve2files")));
 	}
 
-	private static void buildAnalysis(DBUtil dbUtil, Properties props) {
+	private void buildAnalysis(DBUtil dbUtil, Properties props) {
 		throw new IllegalStateException("unimplemented!");
 	}
 }
