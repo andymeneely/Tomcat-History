8f1abd254c41c40e25039111167fb0da3cd100f0
Andy Meneely <andy.meneely@gmail.com>
2012-02-28 09:56:03 -0500

    added logging

--- src/main/java/edu/rit/se/history/tomcat/filter/FilepathFilters.java
+++ src/main/java/edu/rit/se/history/tomcat/filter/FilepathFilters.java
@@ -10,10 +10,12 @@ import org.chaoticbits.devactivity.DBUtil;
 import com.mysql.jdbc.Connection;
 
 public class FilepathFilters {
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(FilepathFilters.class);
 
 	public void filter(DBUtil dbUtil, File filterlist) throws FileNotFoundException, SQLException {
 		Scanner scanner = new Scanner(filterlist);
 		Connection conn = dbUtil.getConnection();
+		log.debug("Going through ignored-filepaths...");
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine().trim();
 			if (line.startsWith("#") || line.length() == 0)
