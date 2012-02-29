757f9af13a0d6f8e1e3fd250a1e1e9b49e383ce0
Andy Meneely <andy.meneely@gmail.com>
2012-02-28 20:02:23 -0500

    automatic download from GoogleDoc. w00t!

--- src/main/java/edu/rit/se/history/tomcat/RebuildHistory.java
+++ src/main/java/edu/rit/se/history/tomcat/RebuildHistory.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.Properties;
 
 import org.apache.log4j.xml.DOMConfigurator;
@@ -12,11 +11,14 @@ import org.chaoticbits.devactivity.DBUtil;
 import org.chaoticbits.devactivity.PropsLoader;
 import org.chaoticbits.devactivity.devnetwork.factory.LoadSVNtoDB;
 
+import com.google.gdata.util.ServiceException;
+
 import edu.rit.se.history.tomcat.filter.FilepathFilters;
 import edu.rit.se.history.tomcat.parse.CVEsParser;
 import edu.rit.se.history.tomcat.parse.FileListingParser;
 import edu.rit.se.history.tomcat.parse.GroundedTheoryResultsParser;
 import edu.rit.se.history.tomcat.parse.VulnerabilitiesToFilesParser;
+import edu.rit.se.history.tomcat.scrapers.GoogleDocExport;
 
 public class RebuildHistory {
 	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(RebuildHistory.class);
@@ -39,6 +41,7 @@ public class RebuildHistory {
 	}
 
 	public void run() throws Exception {
+		downloadGoogleDocs(props);
 		rebuildSchema(dbUtil);
 		// loadSVNXML(dbUtil, props);
 		// filterSVNLog(dbUtil, props);
@@ -60,9 +63,17 @@ public class RebuildHistory {
 
 	private DBUtil setUpDB(Properties props) throws ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
-		DBUtil dbUtil = new DBUtil(props.getProperty("history.dbuser"), props.getProperty("history.dbpw"),
-				props.getProperty("history.dburl"));
+		DBUtil dbUtil = new DBUtil(props.getProperty("history.dbuser"), props.getProperty("history.dbpw"), props.getProperty("history.dburl"));
 		return dbUtil;
+	}
+
+	private void downloadGoogleDocs(Properties props) throws IOException, ServiceException {
+		log.info("Downloading the latest GoogleDocs...");
+		GoogleDocExport export = new GoogleDocExport();
+		export.add(props.getProperty("history.cves.googledoc"), new File(datadir, props.getProperty("history.cves.local")));
+		export.add(props.getProperty("history.cve2files.googledoc"), new File(datadir, props.getProperty("history.cve2files.local")));
+		export.add(props.getProperty("history.groundedtheory.googledoc"), new File(datadir, props.getProperty("history.groundedtheory.local")));
+		export.downloadCSVs(props.getProperty("google.username"), props.getProperty("google.password"));
 	}
 
 	private void rebuildSchema(DBUtil dbUtil) throws FileNotFoundException, SQLException, IOException {
@@ -89,12 +100,12 @@ public class RebuildHistory {
 
 	private void loadGroundedTheoryResults(DBUtil dbUtil, Properties props) throws Exception {
 		log.info("Parsing grounded theory results...");
-		new GroundedTheoryResultsParser().parse(dbUtil, new File(datadir, props.getProperty("history.groundedtheory")));
+		new GroundedTheoryResultsParser().parse(dbUtil, new File(datadir, props.getProperty("history.groundedtheory.local")));
 	}
 
 	private void loadCVEs(DBUtil dbUtil, Properties props) throws Exception {
 		log.info("Parsing CVE details...");
-		new CVEsParser().parse(dbUtil, new File(datadir, props.getProperty("history.cves")));
+		new CVEsParser().parse(dbUtil, new File(datadir, props.getProperty("history.cves.local")));
 	}
 
 	private void filterSVNLog(DBUtil dbUtil, Properties props) {
@@ -103,7 +114,7 @@ public class RebuildHistory {
 
 	private void loadVulnerabilitiesToFiles(DBUtil dbUtil, Properties props) throws Exception {
 		log.info("Parsing CVE to Files...");
-		new VulnerabilitiesToFilesParser().parse(dbUtil, new File(datadir, props.getProperty("history.cve2files")));
+		new VulnerabilitiesToFilesParser().parse(dbUtil, new File(datadir, props.getProperty("history.cve2files.local")));
 	}
 
 	private void optimizeTables(DBUtil dbUtil) throws FileNotFoundException, SQLException, IOException {
--- src/main/java/edu/rit/se/history/tomcat/scrapers/GoogleDocExport.java
+++ src/main/java/edu/rit/se/history/tomcat/scrapers/GoogleDocExport.java
@@ -4,60 +4,59 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.chaoticbits.devactivity.PropsLoader;
 
-import com.google.gdata.client.GoogleAuthTokenFactory.UserToken;
+import com.google.gdata.client.GoogleService;
+import com.google.gdata.client.Service.GDataRequest;
 import com.google.gdata.client.authn.oauth.OAuthException;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
+import com.google.gdata.util.AuthenticationException;
 import com.google.gdata.util.ServiceException;
 
 public class GoogleDocExport {
 
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GoogleDocExport.class);
 	public static final String EXPORT_URL = "https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=@&exportFormat=csv";
 
-	public static void main(String[] args) throws IOException, ServiceException, OAuthException {
-		Properties props = PropsLoader.getProperties("tomcathistory.properties");
+	private List<File> outputFiles = new ArrayList<File>();
+	private List<String> googleDocIDs = new ArrayList<String>();
+
+	public void add(String googleDocID, File outputFile) {
+		outputFiles.add(outputFile);
+		googleDocIDs.add(googleDocID);
+	}
+
+	public void downloadCSVs(String googleUsername, String googlePassword) throws IOException, ServiceException {
 		SpreadsheetService service = new SpreadsheetService("RIT Software Archeology");
-		service.setUserCredentials(props.getProperty("google.username"), props.getProperty("google.password"));
-
-		UserToken docsToken = (UserToken) service.getAuthTokenFactory().getAuthToken();
-		UserToken spreadsheetsToken = (UserToken) service.getAuthTokenFactory().getAuthToken();
-
-		URL metafeedUrl = new URL(EXPORT_URL.replaceFirst("@", "0AutMI5gkPUdLdGpUY1B2Q0ZxUFpZUE1nQWRQd1VzTXc&gid=2"));
-		SpreadsheetFeed feed = service.getFeed(metafeedUrl, SpreadsheetFeed.class);
-
-
-//		
-//		
-//		InputStream inStream = null;
-//		FileOutputStream outStream = null;
-//
-//		try {
-//			inStream = ms.getInputStream();
-//			outStream = new FileOutputStream(new File("c:/data/tomcat/cves.csv"));
-//
-//			int c;
-//			while ((c = inStream.read()) != -1) {
-//				outStream.write(c);
-//			}
-//		} finally {
-//			if (inStream != null) {
-//				inStream.close();
-//			}
-//			if (outStream != null) {
-//				outStream.flush();
-//				outStream.close();
-//			}
-//		}
-
-		// List<SpreadsheetEntry> spreadsheets = feed.getEntries();
-		// for (int i = 0; i < spreadsheets.size(); i++) {
-		// SpreadsheetEntry entry = spreadsheets.get(i);
-		// System.out.println("\t" + entry.getTitle().getPlainText());
-		// }
+		service.setUserCredentials(googleUsername, googlePassword);
+		for (int i = 0; i < googleDocIDs.size(); i++) {
+			log.debug("Downloading: " + googleDocIDs.get(i) + " into " + outputFiles.get(i));
+			URL feedUrl = new URL(EXPORT_URL.replaceFirst("@", googleDocIDs.get(i)));
+			GDataRequest request = service.createFeedRequest(feedUrl);
+			FileOutputStream outStream = null;
+			InputStream inStream = null;
+			try {
+				request.execute();
+				inStream = request.getResponseStream();
+				outStream = new FileOutputStream(outputFiles.get(i));
+				int c;
+				while ((c = inStream.read()) != -1)
+					outStream.write(c);
+			} finally {
+				if (inStream != null) {
+					inStream.close();
+				}
+				if (outStream != null) {
+					outStream.flush();
+					outStream.close();
+				}
+			}
+		}
 	}
 }
