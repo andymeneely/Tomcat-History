6f3f9e10f9703615ea0a1154e3fce68843e308af
Andy Meneely <andy.meneely@gmail.com>
2012-03-09 13:25:48 -0500

    added some histograms to be put into the paper

---
+++ analysis/histograms.R
@@ -0,0 +1,17 @@
+library(RODBC)
+library(lattice)
+conn <- odbcConnect("tomcathistory", uid="tomcathistory", pwd="tomcathistory", case="tolower")
+
+cve <- sqlQuery(conn, "SELECT * FROM CVEResults")
+fixchurn <- sqlQuery(conn, "SELECT * FROM CVEFixChurn")
+files <- sqlQuery(conn, "SELECT * FROM FileResults")
+
+#type="count", col="red", main="Vulnerable NumDevs", freq=TRUE, xlab="Number of Developers"
+
+hist(fixchurn$JavaChurn[fixchurn$JavaChurn!=0], breaks=20, col="red", main="Histogram of Java Code Fix Churn", freq=TRUE, xlab="Lines of Java Code, Added+Deleted", labels=TRUE, axes=FALSE) 
+axis(2) 
+axis(1, at=seq(0,500,20))
+
+hist(fixchurn$JSPChurn[fixchurn$JSPChurn!=0], breaks=4, col="blue", main="Histogram of JSP Code Fix Churn ", freq=TRUE, xlab="Lines of JSP Code, Added+Deleted", labels=TRUE, axes=TRUE) 
+#axis(2) 
+#axis(1, at=seq(0,500,20))
