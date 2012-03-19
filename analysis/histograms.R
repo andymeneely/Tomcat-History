d99d7c979b34fcab68b04847df97840876720665
Andy Meneely <andy.meneely@gmail.com>
2012-03-19 07:53:08 -0400

    some last-minute queries, figures, and stats

--- analysis/histograms.R
+++ analysis/histograms.R
@@ -8,10 +8,15 @@ files <- sqlQuery(conn, "SELECT * FROM FileResults")
 
 #type="count", col="red", main="Vulnerable NumDevs", freq=TRUE, xlab="Number of Developers"
 
-hist(fixchurn$JavaChurn[fixchurn$JavaChurn!=0], breaks=20, col="red", main="Histogram of Java Code Fix Churn", freq=TRUE, xlab="Lines of Java Code, Added+Deleted", labels=TRUE, axes=FALSE) 
+hist(fixchurn$JavaChurn[fixchurn$JavaChurn!=0], breaks=20, col="red", main="Java Code Fix Churn Histogram", freq=TRUE, xlab="Lines of Java Code, Added+Deleted", labels=TRUE, axes=FALSE) 
 axis(2) 
 axis(1, at=seq(0,500,20))
 
-hist(fixchurn$JSPChurn[fixchurn$JSPChurn!=0], breaks=4, col="blue", main="Histogram of JSP Code Fix Churn ", freq=TRUE, xlab="Lines of JSP Code, Added+Deleted", labels=TRUE, axes=TRUE) 
+hist(cve$CVSS, breaks=10, col="blue", main="CVSS Severity", freq=TRUE, xlab="CVSS Score", labels=TRUE, axes=FALSE) 
+axis(2) 
+axis(1, at=seq(0,10,1))
+
+
+#hist(fixchurn$JSPChurn[fixchurn$JSPChurn!=0], breaks=4, col="blue", main="Histogram of JSP Code Fix Churn ", freq=TRUE, xlab="Lines of JSP Code, Added+Deleted", labels=TRUE, axes=TRUE) 
 #axis(2) 
 #axis(1, at=seq(0,500,20))
--- sql/helperQueries.sql
+++ sql/helperQueries.sql
@@ -13,3 +13,16 @@ SELECT CVE, count(distinct Component) NumComponentsAffected
 FROM `tomcathistory`.`cvenonsvnfix`
 group by CVE
 having count(distinct Component) > 1;
+
+/* All subsystems */
+SELECT concat(Component, "/", UtilComponent) AS Subsystem, CVE 
+         FROM cvenonsvnfix
+
+/* All subsystems affected by at least one vulnerablity */
+SELECT Subsystem, count(DISTINCT CVE) NumCVEs
+    FROM
+        (SELECT concat(Component, "/", UtilComponent) AS Subsystem, CVE 
+         FROM cvenonsvnfix) AS Subsystems
+    GROUP BY Subsystem
+    
+   
